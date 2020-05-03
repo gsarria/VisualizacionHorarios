a4f0e05e4eb5e63877707c6ee16c27c173beb164
gsarria <gsarria@javerianacali.edu.co>
2020-05-03 17:40:12 -0500

    Ahora se puede filtrar por programa académico.

--- pagina.py
+++ pagina.py
@@ -51,10 +51,27 @@ def get_db():
         flask.g.sqlite_db = connect_db()
     return flask.g.sqlite_db
 
-
-def procesarCursos(filtroSemestre):
+def procesarProgramas():
     """
-    Función que procesa la información de la base de datos, transformándola en datos para enviar a la página.
+    Función que extrae de la base de datos la información de los programas académicos.
+    Entrada: Ninguna.
+    Salida: Datos procesados.
+    """
+
+    # Accede a la base de datos
+    db = get_db()
+
+    # Recupera (de la base de datos) los programas
+    cur1 = db.execute("select id_carrera, nombre_carrera from definicion")
+    programas = cur1.fetchall()
+
+    # Retorna los programas encontrados en la base de datos
+    return programas
+
+def procesarCursos(filtroSemestre, filtroPrograma):
+    """
+    Función que procesa la información de la base de datos correspondiente a los cursos,
+        transformándola en datos para enviar a la página.
     Entrada: filtroSemestre, variable que representa qué semestre se escogió para visualizar los cursos.
     Salida: Datos procesados.
     """
@@ -62,16 +79,17 @@ def procesarCursos(filtroSemestre):
     # Accede a la base de datos
     db = get_db()
 
-    # Recupera (de la base de datos) los periodos
+    # Recupera (de la base de datos) los cursos, sus profesores y sus horarios
     if(filtroSemestre == 0):
         cur1 = db.execute("select d.codigo, d.nombre, d.grupo, e.nombre, f.dia, f.horainicio, f.horafin \
                            from curso as d, profesor as e, horario as f \
-                           where d.id_profesor = e.id and d.codigo = f.codigo_curso and d.grupo = f.grupo_curso")
+                           where d.id_profesor = e.id and d.codigo = f.codigo_curso \
+                           and d.grupo = f.grupo_curso and d.id_carrera = ?",[filtroPrograma])
     else:
         cur1 = db.execute("select d.codigo, d.nombre, d.grupo, e.nombre, f.dia, f.horainicio, f.horafin \
                            from curso as d, profesor as e, horario as f \
                            where d.id_profesor = e.id and d.codigo = f.codigo_curso and d.grupo = f.grupo_curso \
-                           and d.semestre = ?",[filtroSemestre])
+                           and d.id_carrera = ? and d.semestre = ?",[filtroPrograma, filtroSemestre])
     cursos = cur1.fetchall()
 
     # Listas que contendran las clases que se dictan en dicho dia
@@ -83,6 +101,7 @@ def procesarCursos(filtroSemestre):
     sabado = []
     domingo = []
 
+    # Ciclo para insertar cada horario en la lista apropiada
     for curso in cursos:
         temp = {"codigo":curso[0],
                 "nombre":curso[1],
@@ -90,7 +109,6 @@ def procesarCursos(filtroSemestre):
                 "profesor":curso[3],
                 "horaInicio":curso[5],
                 "span":(curso[6]-curso[5])*2}
-#        print(temp)
         if(curso[4] == "Lunes"):
             lunes.append(temp)
         elif(curso[4] == "Martes"):
@@ -106,9 +124,7 @@ def procesarCursos(filtroSemestre):
         else:
             domingo.append(temp)
 
-#    for i in lunes:
-#        print(i)
-
+    # Retorna los dias agrupados
     return [lunes,martes,miercoles,jueves,viernes,sabado,domingo]
 
 @app.route("/",methods=["GET","POST"])
@@ -120,10 +136,13 @@ def pagina_principal():
     """
 
     filtroSemestre = 0
+    filtroPrograma = 40
     if flask.request.method == 'POST':
         filtroSemestre = int(flask.request.form["filtroSemestre"])
+        filtroPrograma = int(flask.request.form["filtroPrograma"])
 
-    datos = procesarCursos(filtroSemestre)
+    datos = procesarCursos(filtroSemestre,filtroPrograma)
+    programas = procesarProgramas()
     
     return flask.render_template("main.html",datos={"Lunes":datos[0],
                                                     "Martes":datos[1],
@@ -132,8 +151,10 @@ def pagina_principal():
                                                     "Viernes":datos[4],
                                                     "Sabado":datos[5],
                                                     "Domingo":datos[6],
-                                                    "semestre":filtroSemestre})
+                                                    "semestre":filtroSemestre,
+                                                    "programaActual":filtroPrograma,
+                                                    "programas":programas})
 
 
 if(__name__ == "__main__"):
-    app.run(host="127.0.0.1", port=5001)
+    app.run(host="127.0.0.1", port=5013)
